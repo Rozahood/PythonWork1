3b7db1a4b256bb85d417fb2799f12473ad610401
ADMIN <roviobert@gmail.com>
2023-01-11 13:34:27 +0100

    Practice makes perfect

---
+++ String_method.py
@@ -0,0 +1,34 @@
+hello = "Hello there!!!"
+
+#print(hello.upper())
+# print(hello.lower())
+# print(hello.title())
+# print(hello.swapcase())
+# print(hello.casefold())
+# print(hello.capitalize())
+
+
+#print(hello.count("e"))
+
+
+#found =0
+#while True:
+ #   found = hello.find("e", found)
+  #  if found == -1:
+   #     break
+    #print(found)
+
+    #found += 1
+# print(hello.index("a"))print(hello("w"))
+
+#for i in range(1, 11, 2):
+ #   asterick = "*" * i
+  #  print(asterick.center(21))
+
+#print(hello.split())
+#print(hello.partition("e"))
+print("-".join(["a", "b", "C"]))
+
+bin_ = "1011000110010111110"
+#print(bin_.replace("1", "x") .replace("0", "1") .replace("x", "0"))
+print(bin_.translate(str .maketrans("01", "10" "8#!*")))
---
+++ test.py
@@ -0,0 +1,13 @@
+# def add_two_numbers(number1, number2):
+#   print(number1 +number2)
+#  return number1 + number2
+
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+result = first_number + second_number
+
+# this is the main method
+if __name__ == '__main__':
+    print(result)
